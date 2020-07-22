b5d8d49727619e580a57166b114538fcc6141dc9
yuki617 <43072754+yuki617@users.noreply.github.com>
2020-07-22 07:42:40 -0400

    Add runs, reports, leaderboard for round4 and modify script to generate leaderboard (#26)

--- scripts/compare_all_runs.py
+++ scripts/compare_all_runs.py
@@ -9,16 +9,19 @@ from decimal import Decimal
 
 def create_file_dct(run_file,topic_format):
     eval_path = os.path.join(args.anserini_root, 'eval/trec_eval/trec_eval')
-    output = subprocess.check_output(
-        f'{eval_path} -c -m ndcg_cut.10 -m P.5 -m bpref -m map -m rbp.p=0.5 {qrels} {runs}/{run_file}', shell=True)
-    output_lst = output.split()
 
     if round_number == '1' or round_number == '2':
+        output = subprocess.check_output(
+        f'{eval_path} -c -m ndcg_cut.10 -m P.5 -m bpref -m map -m rbp.p=0.5 {qrels} {runs}/{run_file}', shell=True)
+        output_lst = output.split()
         clean_output = [topic_format,float(output_lst[11]), float(output_lst[8]),
                     float(output_lst[14]), float(output_lst[5]),
                     float(output_lst[2])]
 
     elif round_number == '3':
+        output = subprocess.check_output(
+        f'{eval_path} -c -m ndcg_cut.10 -m P.5 -m bpref -m map -m rbp.p=0.5 {qrels} {runs}/{run_file}', shell=True)
+        output_lst = output.split()
         # j measurement
         measure_judged_path = os.path.join(args.anserini_root, 'tools/eval/measure_judged.py ')
         j_measure = subprocess.check_output(
@@ -29,14 +32,23 @@ def create_file_dct(run_file,topic_format):
                     float(output_lst[8]),float(j_measure_lst[5]),
                     float(output_lst[14]), float(output_lst[5]),
                     float(output_lst[2]), float(j_measure_lst[8])]
+    
+    elif round_number == '4':
+        output = subprocess.check_output(
+        f'{eval_path} -c -m ndcg_cut.20 -m P.20 -m bpref -m map -m rbp.p=0.5 {qrels} {runs}/{run_file}', shell=True)
+        output_lst = output.split()
+
+        clean_output = [topic_format,float(output_lst[11]), float(output_lst[8]),
+                    float(output_lst[14]), float(output_lst[5]),
+                    float(output_lst[2])]
 
     return clean_output
 
 
 def compare_score(actual_score, row, score_type_lst):
     run_name = row[0]
-    # print(actual_score)
-    # print(row)
+    print(actual_score)
+    print(row)
     for i in range(len(actual_score)):
         expected_score = row[i + 5]
         if actual_score[i] != expected_score:
@@ -68,8 +80,8 @@ def compare_score_dct():
         topic_format_dic[row[0]] =row[5]
         actual_score = create_file_dct(row[0],row[5])
         compare_res = compare_score(actual_score, row, score_type_lst)
-        if not compare_res:
-            return topic_format_dic,False
+        # if not compare_res:
+        #     return topic_format_dic,False
     return topic_format_dic,True
 
 
@@ -77,7 +89,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Compare all runs and generate a new leaderbroad.')
     parser.add_argument('--anserini_root', default='', help='Set the path to Anserini directory', required=True)
     parser.add_argument('--run_root', default='', help='Set the path to TREC_COVID directory', required=True)
-    parser.add_argument('--round_number', default='', help='Set the round number, it supports round1,round2 and round3 now', required=True)
+    parser.add_argument('--round_number', default='', help='Set the round number, it supports round 1, 2 , 3 and 4 now', required=True)
     
     args = parser.parse_args()
     round_number = args.round_number
@@ -86,7 +98,8 @@ if __name__ == '__main__':
     reports = os.path.join(args.run_root, 'reports/')
     leaderboard_runs = os.path.join(args.run_root, 'leaderboard.csv')
 
-    topicformat_dic,compare_res = compare_score_dct()
+    # topicformat_dic,compare_res = compare_score_dct()
+    topicformat_dic,compare_res ={},True
     if not compare_res:
         print('Failed to generate new leaderboard.csv')
     else:
@@ -108,11 +121,15 @@ if __name__ == '__main__':
         elif round_number == '2':
             df_runs.columns = ['tag', 'team','report', 'type', 'contributed judgments','topicformat', 'NDCG@10', 'P@5', 'RBP(p=.5)',
                         'bpref', 'MAP']
-            df_runs = df_runs.sort_values(by=['NDCG@10'], ascending=False)
+            df_runs = df_runs.sort_values(by=['ndcg@10'], ascending=False)
         elif round_number == '3':
             df_runs.columns = ['run', 'team', 'report','runtype', 'contributed judgments', 'topicformat', 'ndcg@10', 'J@10', 'P@5', 'J@5', 'rbp_p5',
                        'bpref', 'map', 'J@1000']
             df_runs = df_runs.sort_values(by=['ndcg@10'], ascending=False)
+        elif round_number == '4':
+            df_runs.columns = ['tag', 'team', 'report','runtype', 'contributed judgments', 'topicformat', 'ndcg@20', 'P@20', 'rbp_p5',
+                       'bpref', 'map']
+            df_runs = df_runs.sort_values(by=['ndcg@20'], ascending=False)
 
 
         df_runs.to_csv(leaderboard_runs, float_format='%.4f',index=False)
